edf78faf28d1b658250dbb700931f262a0de47ec
zamfofex <zamfofex@twdb.moe>
2022-02-06 04:42:59 -0300

    experiment with the AI algorithm

--- internal/analysis.js
+++ internal/analysis.js
@@ -1,5 +1,4 @@
 let depth = 4
-let spread = 16
 
 export let traverse = (turn, board, i) =>
 {
@@ -20,38 +19,31 @@ export let traverse = (turn, board, i) =>
 		return [boardScore + Math.random() - 0.5]
 	}
 	
-	let captures = []
-	let next = Array(moves.length).fill()
-	for (let [j, move] of moves.entries())
+	let next = moves
+	
+	if (i > 1)
 	{
-		move.play()
-		
-		let moveScore = board.getScore()
-		let info = {move, score: moveScore + Math.random() - 0.5}
-		if ((i % 2 === 0) !== (turn === "black")) info.score *= -1
-		
-		if (moveScore !== boardScore) captures.push(info)
-		next[j] = info
-		
-		move.unplay()
+		next = []
+		for (let move of moves)
+		{
+			move.play()
+			if (board.isCheck() || board.getScore() !== boardScore)
+				next.push(move)
+			move.unplay()
+		}
 	}
-	
-	next.sort((a, b) => b.score - a.score)
-	next.length = Math.min(next.length, Math.round(spread * (depth - i) / depth))
-	
-	next.push(...captures)
 	
 	let score
 	let minimax
 	
-	if	(i % 2 === 0)
+	if (i % 2 === 0)
 		score = Infinity,
 		minimax = Math.min
 	else
 		score = -Infinity,
 		minimax = Math.max
 	
-	for (let {move} of new Set(next))
+	for (let move of next)
 	{
 		move.play()
 		score = minimax(score, traverse(turn, board, i + 1)[0])
